69ffb8c08ec01820f0b2c8bfb2c73e228bbc3162
Kalpana Joyce Dvari <dovarikalpanajoyce@gmail.com>
2026-03-20 22:37:43 +0000

    Updated PWA + mobile app setup

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,30 @@
+const CACHE_NAME = "solite-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/login.html",
+  "/profile.html",
+  "/saved.html",
+  "/playlist.html",
+  "/cafes.html",
+  "/music.html",
+  "/stories.html",
+  "/images/book.jpg",
+  "/images/cafe.jpg",
+  "/images/series.jpg"
+];
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("solite-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
