1855975bc81db9266ba6c2fda8572abb56db529a
Kalpana Joyce Dvari <dovarikalpanajoyce@gmail.com>
2026-03-29 21:50:14 +0100

    add firebase config for GitHub Pages

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDc8vbutr2Sfnb49D58LYDdcxAqdY3sNWo',
+  authDomain: 'solite-d9bdb.firebaseapp.com',
+  projectId: 'solite-d9bdb',
+  storageBucket: 'solite-d9bdb.appspot.com',
+  messagingSenderId: '933108708137',
+  appId: '1:933108708137:web:e443d5045935fac50cbd38'
+};
